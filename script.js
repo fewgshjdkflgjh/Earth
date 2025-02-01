77f044e9f7b914d08c8d2de2c5c7dfe6a3d8089c
fewgshjdkflgjh <140098581+fewgshjdkflgjh@users.noreply.github.com>
2025-02-01 19:16:43 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const GITHUB_USERNAME = "fewgshjdkflgjh"; // Replace with your GitHub username
 const REPO_NAME = "Earth"; // Replace with your repo name
 const FILE_PATH = "roads.json";
-const ACCESS_TOKEN = "your-github-access-token"; // Replace with your token
+const ACCESS_TOKEN = "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"; // Replace with your token
 
 const GITHUB_API_URL = `https://api.github.com/repos/${GITHUB_USERNAME}/${REPO_NAME}/contents/${FILE_PATH}`;
 
